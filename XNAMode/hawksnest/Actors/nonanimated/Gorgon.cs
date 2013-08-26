bd2aac07b387bf263e379a6bcb0c82fe536959f8
Shane Brouwer <shane@findesign.com.au>
2013-08-26 10:40:37 +1000

    fr_actors

--- XNAMode/hawksnest/Actors/nonanimated/Gorgon.cs
+++ XNAMode/hawksnest/Actors/nonanimated/Gorgon.cs
@@ -18,17 +18,17 @@ namespace XNAMode
 
             actorName = "The TemplateActor";
 
-            loadGraphic(FlxG.Content.Load<Texture2D>("initials/gorgon_11x24"), true, false, 11, 24);
+            loadGraphic(FlxG.Content.Load<Texture2D>("initials/allActors"), true, false, 26, 26);
 
-            addAnimation("run", new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }, 12);
+            //addAnimation("run", new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }, 12);
             addAnimation("idle", new int[] { 0 }, 12);
-            addAnimation("attack", new int[] { 2, 4 }, 18);
+            //addAnimation("attack", new int[] { 2, 4 }, 18);
 
             //bounding box tweaks
-            width = 7;
+            width = 20;
             height = 20;
-            offset.X = 2;
-            offset.Y = 4;
+            offset.X = 3;
+            offset.Y = 6;
 
             //basic player physics
             int runSpeed = 30;
