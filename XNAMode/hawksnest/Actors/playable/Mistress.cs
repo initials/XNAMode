641897417f1de361c27afdeae88384440fcaeb63
Shane Brouwer <initials@initialscommand.com>
2013-08-27 23:10:17 +1000

    whips faster

--- XNAMode/hawksnest/Actors/playable/Mistress.cs
+++ XNAMode/hawksnest/Actors/playable/Mistress.cs
@@ -23,7 +23,7 @@ namespace XNAMode
 
             addAnimation("run", new int[] { 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, 12);
             addAnimation("idle", new int[] { 0 }, 12);
-            addAnimation("attack", new int[] { 0, 1, 2, 3, 4, 5, 6 }, 18);
+            addAnimation("attack", new int[] { 0, 1, 2, 3, 4, 5, 6 }, 30);
 
             //bounding box tweaks
             width = 9;
