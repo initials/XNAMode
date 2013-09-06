5d7f5575837b99e7edf9045e045b2912951a7c86
Shane Brouwer <shane@findesign.com.au>
2013-09-06 13:25:15 +1000

    frames all correct

--- XNAMode/homewreckr/Actors/Actor.cs
+++ XNAMode/homewreckr/Actors/Actor.cs
@@ -15,94 +15,94 @@ namespace XNAMode
         // A bunch of helpers for the allActors sprite sheet.
 
         // Row:  0
-        //public const int FR_artist = 0;
-        //public const int FR_assassin = 1;
+        public const int FR_soldier = 0;
+        public const int FR_paladin = 1;
         public const int FR_executor = 2;
-        //public const int FR_bat = 3;
-        //public const int FR_blight = 4;
-        //public const int FR_bloatedzombie = 5;
+        public const int FR_savage = 3;
+        public const int FR_marksman = 4;
+        public const int FR_monk = 5;
         public const int FR_assassin = 6;
         public const int FR_corsair = 7; 
         // Row:  1
-        //public const int FR_centaur = 8;
-        //public const int FR_chicken = 9;
-        //public const int FR_chimaera = 10;
+        public const int FR_wizard = 8;
+        public const int FR_priest = 9;
+        public const int FR_warlock = 10;
         public const int FR_druid = 11; 
-        //public const int FR_cow = 12;
-        //public const int FR_cyclops = 13;
+        public const int FR_merchant = 12;
+        public const int FR_mechanic = 13;
         public const int FR_artist = 14; 
-        //public const int FR_deer = 15;
+        public const int FR_gormet = 15;
         // Row:  2
         public const int FR_chicken = 16;
-        //public const int FR_djinn = 17;
-        //public const int FR_drone = 18;
+        public const int FR_rat = 17;
+        public const int FR_toad = 18;
         public const int FR_feline = 19;
-        //public const int FR_dwarf = 20;
+        public const int FR_sheep = 20;
         public const int FR_cow = 21;
-        //public const int FR_executor = 22;
+        public const int FR_horse = 22;
         public const int FR_dear = 23;
         // Row:  3
-        //public const int FR_floatingeye = 24;
+        public const int FR_snake = 24;
         public const int FR_bat = 25;
-        //public const int FR_gelatine = 26;
-        //public const int FR_gloom = 27;
-        //public const int FR_glutton = 28;
-        //public const int FR_goblin = 29;
-        //public const int FR_golem = 30;
-        //public const int FR_gorgon = 31;
+        public const int FR_zinger = 26;
+        public const int FR_spider = 27;
+        public const int FR_wolf = 28;
+        public const int FR_grizzly = 29;
+        public const int FR_lion = 30;
+        public const int FR_unicorn = 31;
         // Row:  4
-        //public const int FR_gourmet = 32;
-        //public const int FR_cyclops = 33;
+        public const int FR_troll = 32;
+        public const int FR_cyclops = 33;
         public const int FR_dwarf = 34;
-        //public const int FR_harvester = 35;
-        //public const int FR_horse = 36;
-        //public const int FR_ifrit = 37;
-        //public const int FR_imp = 38;
+        public const int FR_goblin = 35;
+        public const int FR_ogre = 36;
+        public const int FR_nymph = 37;
+        public const int FR_mermaid = 38;
         public const int FR_centaur = 39; 
         // Row:  5
-        //public const int FR_lich = 40;
-        //public const int FR_lion = 41;
-        //public const int FR_fungant = 42;
-        //public const int FR_mechanic = 43;
-        //public const int FR_mephisto = 44;
+        public const int FR_tauro = 40;
+        public const int FR_treant = 41;
+        public const int FR_fungant = 42;
+        public const int FR_zombie = 43;
+        public const int FR_mummy = 44;
         public const int FR_deathclaw = 45; 
         public const int FR_bloatedzombie = 46; 
-        //public const int FR_mimick = 47;
+        public const int FR_vampire = 47;
         // Row:  6
         public const int FR_bogbeast = 48; 
-        //public const int FR_blight = 49;
-        //public const int FR_nightmare = 50;
-        //public const int FR_nymph = 51;
-        //public const int FR_ogre = 52;
-        public const int FR_gloom = 53; 
-        //public const int FR_phantom = 54;
-        //public const int FR_priest = 55;
+        public const int FR_blight = 49;
+        public const int FR_willowisp = 50;
+        public const int FR_phantom = 51;
+        public const int FR_gloom = 52;
+        public const int FR_nightmare = 53; 
+        public const int FR_skeleton = 54;
+        public const int FR_grimwarrior = 55;
         // Row:  7
-        //public const int FR_prism = 56;
-        //public const int FR_rat = 57;
-        //public const int FR_savage = 58;
-        //public const int FR_devil = 59;
-        //public const int FR_sheep = 60;
-        //public const int FR_skeleton = 61;
-        //public const int FR_snake = 62;
-        //public const int FR_soldier = 63;
+        public const int FR_harvester = 56;
+        public const int FR_lich = 57;
+        public const int FR_imp = 58;
+        public const int FR_devil = 59;
+        public const int FR_mephisto = 60;
+        public const int FR_kerberos = 61;
+        public const int FR_glutton = 62;
+        public const int FR_tormentor = 63;
         // Row:  8
-        //public const int FR_sphinx = 64;
-        //public const int FR_spider = 65;
-        //public const int FR_succubus = 66;
+        public const int FR_succubus = 64;
+        public const int FR_gorgon = 65;
+        public const int FR_ifrit = 66;
         public const int FR_embersteed = 67; 
-        //public const int FR_toad = 68;
-        //public const int FR_bombling = 69;
+        public const int FR_seraphine = 68;
+        public const int FR_bombling = 69;
         public const int FR_drone = 70; 
-        //public const int FR_troll = 71;
+        public const int FR_mimick = 71;
         // Row:  9
         public const int FR_automaton = 72; 
-        //public const int FR_vampire = 73;
+        public const int FR_golem = 73;
         public const int FR_gelantine = 74; 
         public const int FR_floatingeye = 75; 
-        //public const int FR_wizard = 76;
+        public const int FR_prism = 76;
         public const int FR_djinn = 77;
-        //public const int FR_zinger = 78;
+        public const int FR_sphinx = 78;
         public const int FR_chimera = 79;
 
 
--- XNAMode/homewreckr/Actors/nonanimated/Bogbeast.cs
+++ XNAMode/homewreckr/Actors/nonanimated/Bogbeast.cs
@@ -21,7 +21,7 @@ namespace XNAMode
             loadGraphic(FlxG.Content.Load<Texture2D>("initials/allActors"), true, false, 26, 26);
 
             //addAnimation("run", new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }, 12);
-            addAnimation("idle", new int[] { Actor.FR_bogbeast }, 12);
+            addAnimation("idle", new int[] { FR_bogbeast }, 12);
             //addAnimation("attack", new int[] { 2, 4 }, 18);
 
             //bounding box tweaks
