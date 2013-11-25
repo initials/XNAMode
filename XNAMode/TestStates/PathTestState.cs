745dd08e37b348b6063429aaade8a787b5a93d27
Shane Brouwer <shane@findesign.com.au>
2013-11-25 15:59:41 +1100

    x

--- XNAMode/TestStates/PathTestState.cs
+++ XNAMode/TestStates/PathTestState.cs
@@ -10,35 +10,42 @@ using System.Xml.Linq;
 
 namespace XNAMode
 {
+    /// <summary>
+    /// Shows how to use a path object, and follow.
+    /// </summary>
     public class PathTestState : FlxState
     {
+        /// <summary>
+        /// A sprite to follow the path.
+        /// </summary>
         FlxSprite collider;
 
         override public void create()
         {
             base.create();
 
+            // Create the path.
             FlxPath path = new FlxPath(null);
             path.add(10, 45);
             path.add(45, 45);
             path.add(66, 66);
             path.add(77, 77);
             path.add(88, 0);
-            //collider.path = path;
 
+
+            // Create an object to follow the path.
             collider = new FlxSprite(0, 0);
             collider.loadGraphic(FlxG.Content.Load<Texture2D>("initials/autotiles_16x16"), false, false, 8, 8);
             add(collider);
-            //collider.acceleration.Y = 5;
-            collider.followPath(path, 151.0f, FlxObject.PATH_YOYO, false);
+
+            // Tell the sprite to follow the path. 
+            collider.followPath(path, 15.0f, FlxObject.PATH_YOYO, false);
 
         }
 
 
         override public void update()
         {
-            //if (FlxG.keys.A) collider.thrust += 5;
-
             base.update();
         }
     }
