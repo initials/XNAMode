687eb691f440ef2bffc5dd174022c6312d71db44
Shane Brouwer <initials@initialsgames.com>
2013-12-11 00:31:01 +1100

    x

--- XNAMode/TestStates/VCRState.cs
+++ XNAMode/TestStates/VCRState.cs
@@ -15,12 +15,67 @@ namespace XNAMode
     {
         string _data;
         int _frame;
+        Marksman m;
+        Mistress miss;
+
+        Dictionary<string, string> levelAttrs;
+        List<Dictionary<string, string>> actorsAttrs;
+        private FlxTilemap destructableTilemap;
+        private FlxSprite collider;
 
         override public void create()
         {
             FlxG.mouse.show(FlxG.Content.Load<Texture2D>("Mode/cursor"));
 
+
+
             base.create();
+
+
+            levelAttrs = new Dictionary<string, string>();
+
+            levelAttrs = FlxXMLReader.readAttributesFromTmxFile("Lemonade/levels/military_level1.tmx", "map");
+
+            foreach (KeyValuePair<string, string> kvp in levelAttrs)
+            {
+                //Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+
+            actorsAttrs = FlxXMLReader.readNodesFromTmxFile("Lemonade/levels/military_level1.tmx", "map", "bg");
+            foreach (Dictionary<string, string> nodes in actorsAttrs)
+            {
+                foreach (KeyValuePair<string, string> kvp in nodes)
+                {
+                    //Console.Write("Key = {0}, Value = {1}, ", kvp.Key, kvp.Value);
+                }
+                //Console.Write("\r\n");
+            }
+
+            // TMX fixes. kill newlines.
+            string newStringx = actorsAttrs[0]["csvData"].Replace(",\n", "\n");
+            newStringx = newStringx.Remove(0, 1);
+            newStringx = newStringx.Remove(newStringx.Length - 1);
+
+
+            destructableTilemap = new FlxTilemap();
+            destructableTilemap.auto = FlxTilemap.STRING;
+
+            // TMX maps have indexOffset of -1;
+            destructableTilemap.indexOffset = -1;
+            destructableTilemap.loadMap(newStringx, FlxG.Content.Load<Texture2D>("Lemonade/tiles_sydney"), 20, 20);
+            destructableTilemap.boundingBoxOverride = true;
+            add(destructableTilemap);
+
+
+
+
+            m = new Marksman(40, 40, null);
+            m.isPlayerControlled = true;
+            add(m);
+
+            miss = new Mistress(100, 40);
+            add(miss);
+
         }
 
         override public void update()
@@ -36,6 +91,10 @@ namespace XNAMode
 
                 SaveToDevice(_data, "replay" + nowTime  + ".txt");
             }
+
+
+            FlxU.collide(m, destructableTilemap);
+            FlxU.collide(miss, destructableTilemap);
 
 
             base.update();
