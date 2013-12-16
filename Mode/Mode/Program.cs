829c46c5c05d7a570ca1d843892370c35b716267
Shane Brouwer <shane@findesign.com.au>
2013-12-16 12:47:04 +1100

    added a build for Mode

---
+++ Mode/Mode/Program.cs
@@ -0,0 +1,60 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// Flixel for XNA.
+// Original repo : https://github.com/StAidan/X-flixel
+// Extended and edited repo : https://github.com/initials/XNAMode
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+
+namespace Mode
+{
+    /// <summary>
+    /// Flixel enters here.
+    /// <code>FlxFactory</code> refers to it as the "masterclass".
+    /// </summary>
+    public class FlixelEntryPoint2 : FlxGame
+    {
+        public FlixelEntryPoint2(Game game)
+            : base(game)
+        {
+
+            int w = 320;
+            int h = 180;
+
+            initGame(w, h, new XNAMode.MenuState(), new Color(15, 15, 15), true, new Color(5, 5, 5));
+
+
+            //initGame(w, h, new EmptyIntroTestState(), new Color(15, 15, 15), false, new Color(5, 5, 5));
+
+            //initGame(w, h, new BasePlayStateFromOel(), new Color(15, 15, 15), false, new Color(5, 5, 5));
+
+            //initGame(w, h, new CharacterSelectionState(), new Color(15, 15, 15), true, new Color(5, 5, 5));
+
+            //initGame(w, h, new BasePlayStateFromOel(), new Color(15, 15, 15), false, new Color(5, 5, 5));
+
+
+            //initGame(w, h, new PathTestState(), new Color(15, 15, 15), false, new Color(5, 5, 5));
+
+
+            //initGame(w, h, new MenuState(), new Color(15, 15, 15), false, new Color(5, 5, 5));
+
+
+
+            FlxG.debug = true;
+            FlxG.zoom = 1280 / w;
+            FlxG.level = -1;
+
+            //FourChambers_Globals.BUILD_TYPE = FourChambers_Globals.BUILD_TYPE_RELEASE;
+            //FourChambers_Globals.DEMO_VERSION = false;
+            //FourChambers_Globals.PIRATE_COPY = false;
+
+        }
+    }
+}
