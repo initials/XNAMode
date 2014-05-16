7d0f80864bb348b29f3f92f4c27069e73a24f87b
Shane Brouwer <initials@initialsgames.com>
2014-05-16 10:14:25 +1000

    x

---
+++ XNAMode/Lemonade/extra/Hud.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Lemonade
+{
+    class Hud : FlxSprite
+    {
+
+        public Hud(int xPos, int yPos)
+            : base(xPos, yPos)
+        {
+            loadGraphic(FlxG.Content.Load<Texture2D>("Lemonade/currentChar"), true, false, 28, 28);
+
+            addAnimation("andre", new int[] { 0 }, 0, true);
+            addAnimation("liselot", new int[] { 1 }, 0, true);
+
+            play("andre");
+        }
+
+        override public void update()
+        {
+
+
+            base.update();
+
+        }
+
+
+    }
+}
