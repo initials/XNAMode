2939246bdbbd4fe3da1c1d3dc1106ff42a82874c
Shane Brouwer <initials@initialsgames.com>
2014-07-19 12:38:53 +1000

    ready to go

--- SuperLemonadeFactory/SuperLemonadeFactory/Program.cs
+++ SuperLemonadeFactory/SuperLemonadeFactory/Program.cs
@@ -35,7 +35,8 @@ namespace Loader_SuperLemonadeFactory
             FlxG.debug = false;
             FlxG.level = 1;
 
-            Lemonade.Lemonade_Globals.PAID_VERSION = Lemonade.Lemonade_Globals.PIRATE_MODE;
+            //Lemonade.Lemonade_Globals.PAID_VERSION = Lemonade.Lemonade_Globals.PIRATE_MODE;
+            Lemonade.Lemonade_Globals.PAID_VERSION = Lemonade.Lemonade_Globals.FULL_MODE;
 
 
             FlxG.BUILD_TYPE = FlxG.BUILD_TYPE_PC;
